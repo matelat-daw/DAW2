6ec352c7a488331ee2476a62e869e5cf1f137d6c
Orion68 <orions68@gmail.com>
2025-02-03 19:40:38 +0000

    Update 3-2-2025 19:40

--- DEW/React/Project-Recipies/src/services/Service.jsx
+++ DEW/React/Project-Recipies/src/services/Service.jsx
@@ -1,19 +1,11 @@
 import toast from './js/script.js';
 
-// let alumnos = [
-//     { id: 1, nombre: 'Juan', grupo: 'A' },
-//     { id: 2, nombre: 'María', grupo: 'B' },
-//     { id: 3, nombre: 'Carlos', grupo: 'A' }
-// ];
-
-let alumnos2 = await fetch("http://localhost:3000/alumnos").then(respuesta => respuesta.json())
+let recetas = await fetch("http://localhost:3000/recetas").then(respuesta => respuesta.json())
                 .catch(respuesta => toast(2, "Error de Conexión", "Lo Siento No hay Conexión con el Servidor. Asegurate de que el Servidor está en Ejecución. Error: " + respuesta))
                 // .then(respuesta => toast(0, "Todo Ha Ido Bien:", "Se han cargado los Datos del Servidor." + respuesta))
 
-const grupos = ["A", "B"];
-
 // Obtener todos los alumnos
-export const getAlumnos = async () => {
+export const getRecetas = async () => {
     try {
       return alumnos2;
     } catch (error) {
@@ -22,42 +14,41 @@ export const getAlumnos = async () => {
   };
 
   // Obtener todos los grupos
-export const getGrupos = async () => {
-    try {
-      return grupos;
-    } catch (error) {
-      console.error('Error fetching items:', error);
-    }
-  };
+// export const getGrupos = async () => {
+//     try {
+//       return grupos;
+//     } catch (error) {
+//       console.error('Error fetching items:', error);
+//     }
+//   };
   
-  // Crear un nuevo alumno
-  export const createAlumno = async (alumno) => {
+  // Crear una nueva Receta
+  export const createRecta = async (receta) => {
     try {
-      alumno.id = alumnos2.length ? alumnos2[alumnos2.length - 1].id + 1 : 1;
-      alumnos2.push(alumno);
-      return alumnos2;
+      receta.id = recetas.length ? recetas[recetas.length - 1].id + 1 : 1;
+      recetas.push(receta);
+      return recetas;
     } catch (error) {
       console.error('Error creating item:', error);
     }
   };
   
-  // Actualizar un alumno existente
-  export const updateAlumno = async (id, updatedAlumno) => {
+  // Actualizar una Receta Existente
+  export const updateReceta = async (id, updatedReceta) => {
     try {
-      alumnos2 = alumnos2.map(alumno => 
-        alumno.id === id ? { ...alumno, ...updatedAlumno } : alumno
+      recetas = recetas.map(receta => 
+        receta.id === id ? { ...receta, ...updatedReceta } : receta
       );
-    //   return updatedAlumno;
-    return alumnos2;
+    return recetas;
     } catch (error) {
       console.error('Error updating item:', error);
     }
   };
   
   // Eliminar un alumno
-  export const deleteAlumno = async (id) => {
+  export const deleteReceta = async (id) => {
     try {
-        alumnos2 = alumnos2.filter(alumno => alumno.id !== id);
+        recetas = recetas.filter(receta => receta.id !== id);
     } catch (error) {
       console.error('Error deleting item:', error);
     }
