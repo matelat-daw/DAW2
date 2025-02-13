3743bd16454d64686327829a819fffa45a729f6c
matelat-daw <cesarmatelat@gmail.com>
2025-02-13 09:58:16 +0000

    Update 13-2-2025 9:58

--- DEW/React/Project-Recipies - Imágenes/src/Inicio.jsx
+++ DEW/React/Project-Recipies - Imágenes/src/Inicio.jsx
@@ -6,7 +6,7 @@ export default function Inicio() {
   return (
     <>
         <h1>Te Damos la Bienvenida a la Base de Datos de Recetas del CIFP César Manrique</h1>
-        <ul>
+        <ul className='none'>
             <li><Link to="/read">Muestra la Lista de Recetas</Link></li>
             <li><Link to="/Create">Agrega una Receta a la Lista</Link></li>
         </ul>
